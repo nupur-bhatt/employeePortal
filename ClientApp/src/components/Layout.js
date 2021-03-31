f430329c10b556d7f67fcca6bf2122f5105237ef
Nupur Bhatt <Nupur.Bhatt@dal.ca>
2021-03-31 12:33:32 -0300

    Frontend able to send HTTP POST, GET and DELETE requests

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Features from './Features';
 import './materialtheme.css';
-import { Container } from '@material-ui/core';
 
 export class Layout extends Component {
   static displayName = Layout.name;
